ed60210f7b5a52df7abe24dee6e4a8631c00be85
Wilsonrangari <rangariwilson@gmail.com>
2024-06-21 13:12:39 +0200

    new update

--- Project2/index.js
+++ Project2/index.js
@@ -3,3 +3,6 @@
 const fs = require('fs')
 
 fs.writeFileSync("hello.txt","Hi Wilson automatically this text will be added to the hello.txt")
+
+console.log("-->",__dirname)
+console.log("-->",__filename)
